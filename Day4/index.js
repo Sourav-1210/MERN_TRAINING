f7877f707aff412d8aecd21845627829a6a9bc4b
Sourav-1210 <souravsharma9142@gmail.com>
2026-01-24 10:55:46 +0530

    Learn Basic of javascript

---
+++ Day4/index.js
@@ -0,0 +1,16 @@
+function area(width,height){
+    const ans = width*height;
+    console.log(ans);
+}
+
+console.log(area(5,4));
+
+function check(n){
+    if(n%2==0){
+        console.log("Even");
+    }else{
+        console.log("Odd");
+    }
+}
+
+console.log(check(5));
