37daa7ce68feb47b5407c8a11b2417f4eba6d899
Sourav-1210 <souravsharma9142@gmail.com>
2026-01-28 11:50:10 +0530

    learn different function higher order function

--- Week2/Day2/HigherOrder.js
+++ Week2/Day2/HigherOrder.js
@@ -44,19 +44,71 @@
 
 // console.log(newArray);
 
-let arr = [1,2,1,3,3,5,"Hii","Hello",1,1];
-function counter(arr){
-    let freq = {};
-    for(let i=0;i<arr.length;i++){
-        if(freq[arr[i]]){
-            freq[arr[i]]++;
-        }else{
-            freq[arr[i]] = 1;
-        }
-    }
-    return freq;
+// let arr = [1,2,1,3,3,5,"Hii","Hello",1,1];
+// function counter(arr){
+//     let freq = {};
+//     for(let i=0;i<arr.length;i++){
+//         if(freq[arr[i]]){
+//             freq[arr[i]]++;
+//         }else{
+//             freq[arr[i]] = 1;
+//         }
+//     }
+//     return freq;
 
-}
+// }
 
-let result = counter(arr);
-console.log(result);
+// let result = counter(arr);
+// console.log(result);
+
+// let arr = ["hello world","good day"];
+// let newArray = arr.map(str =>
+//     str.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join("")
+// )
+// console.log(newArray);
+
+// let names = ["sourav","shivang","tanishk"];
+// let array = names.map(name=>name[0].toUpperCase() + name.slice(1));
+// console.log(array);
+
+// let arr = [1,5,2,88,4];
+// let newArray = arr.filter(num => num%2==0);
+// console.log(newArray);/
+
+// let str = ["sourav"," ","Tanishk","      "];
+// str = str.filter(name=>name.trim().length!==0);
+// console.log(str);
+
+// let users = [
+//     {name : "Aman",age :"16"},
+//     {name : "Riya",age :"21"},
+//     {name : "Kunal",age :"17"},
+//     {name : "Neha",age:"25"}
+// ]
+
+// let arr = users.filter(value => value.age>18)
+
+// console.log(arr);
+
+// let words = ["Hello","world","JavaScript","map","Code"];
+// let arr = words.filter(str => str[0] === str[0].toUpperCase());
+// console.log(arr);
+
+// let arr = [1,2,3,4];
+// let sum = arr.reduce((acc,curr) => acc+curr);
+// console.log(sum);
+
+// let arr = [1,2,3,4];
+// let prod = arr.reduce((acc,curr) => acc*curr,1);
+// console.log(prod);
+
+let arr = ["apple","banana","apple","orange","banana","apple"];
+let freq = arr.reduce((acc,curr)=>{
+    acc[curr] = (acc[curr]||0)+1;
+    return acc;
+},{});
+console.log(freq);
+
+
+
+
